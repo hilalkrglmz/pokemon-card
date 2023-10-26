88c7d3931b6994c6cb77f8572e01dcfb3e1a7d07
hilalkrglmz <hllkrg6@gmail.com>
2023-10-26 09:48:45 +0300

    project done

---
+++ src/Pokecard.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+
+
+const POKE_API =
+"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
+
+class PokeCard extends Component {
+
+    
+    render(){
+        let imgSrc = `${POKE_API}${this.props.id}.png`
+        return(
+            <div className="PokeCard">
+                <h3 className="Pokecard-title">{this.props.name}</h3>
+                <div className="Pokecard-image">
+                    <img src={imgSrc} alt="{this.props.name}" />
+                </div>
+                <div className="Pokecard-data">Type: {this.props.type}</div>
+                <div className="Pokecard-data">Exp: {this.props.exp}</div>
+
+            </div>
+        )
+    }
+
+}
+
+
+export default PokeCard;
